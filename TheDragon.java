a48759c0abaf9bfc4ce7f34349478493c69277f4
Shemamforash <samwest2@hotmail.co.uk>
2014-10-18 18:28:10 +0100

    Commit 5- Spiro Improvements
    Changed set up of TheDragon class. When class is instantiated all you
    need to pass is width and height of the JFrame. If we want to resize the
    window we can sort that out a bit later. To make a Spiro object just
    instantiate the class (it's nested atmo but if that is a problem you can
    just take it out). The Spiro class takes some important constructors
    that you NEED to pass into it. If you have other variables you want to
    pass in let me know and I'll fix the script. Other than that the only
    thing you need to know is that the script only supports 1 Spiro at the
    moment, and if you want the buffered image you just have to call
    getImage which will return a tasty image for you to draw to the screen.
    I've introduced delta time into it so the animation should be silky
    smooth. GetColour() probably doesn't work since I couldn't test it, but
    pls don't mess around with my logic :P

--- TheDragon.java
+++ TheDragon.java
@@ -1,91 +1,135 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package spiro;
-
 import java.awt.*;
 import java.awt.image.BufferedImage;
+
 import javax.swing.*;
+
 import java.util.ArrayList;
 
 public class TheDragon extends JInternalFrame {
 
     Graphics buffer; //Used for double buffering
     BufferedImage offScreen; //Image that buffer will draw
-    private int paintIterator = 0; //Keep track of the number of pixels you have drawn
-    private ArrayList<spiroObject> allSpiros = new ArrayList<spiroObject>(); //Array to contain all spiros in case we draw multiple objects
+    private int paintIterator = 100; //Keep track of the number of pixels you have drawn
+    private float last_Time = 0f, delta_Time = 0f, pixels_Per_Sec = 500;
+    private SpiroObject current_Spiro; //Array to contain all spiros in case we draw multiple objects
 
-
-    public TheDragon(JInternalFrame Jf) //First method called
+    public TheDragon(int width, int height) //First method called
     {
-        Jf.add(this);
-        int width = Jf.getWidth();
-        int height = Jf.getHeight();
         offScreen = (BufferedImage) createImage(width, height); //Some arbitrary screen size TODO: change this to screen dimensions
         buffer = offScreen.getGraphics(); //Assign the buffer to get the graphics from off screen
     }
+    
+    private Color getColour()
+    {
+    	float current_Pos = (1f / (float)current_Spiro.x_Vals.size()) * (float)paintIterator;
+    	Color a = null, b = null;
+    	
+    	for(int i = 0; i < current_Spiro.spiro_Colours.size(); ++i)
+    	{
+    		if(current_Pos * (float)current_Spiro.spiro_Colours.size() <= i)
+    		{    			
+    			a = current_Spiro.spiro_Colours.get(i - 1);
+    			b = current_Spiro.spiro_Colours.get(i);
+    			float percentage_Colour_A = (1f / ((float)current_Spiro.x_Vals.size() / current_Spiro.spiro_Colours.size()) * i - 1) * (float)paintIterator;
+    			float percentage_Colour_B = (1f / ((float)current_Spiro.x_Vals.size() / current_Spiro.spiro_Colours.size()) * i) * (float)paintIterator;
 
-    public void paint(Graphics g, Color c) {
-        buffer.clearRect(0, 0, 400, 400); //Clear screen
-        buffer.setColor(c); //Set colour of pixels to orange TODO: Make the colour change
+    			current_Pos = (1f / (current_Pos - (float)current_Spiro.x_Vals.size() / current_Spiro.spiro_Colours.size()) * i) * (float)paintIterator;
+    			break;
+    		}
+    	}
+    	
+    	Color current_Hue = null;
+    	
+    	if(a != null && b != null)
+    	{
+    		float temp_R = ((((1f / 255) * a.getRed()) - ((1f / 255) * b.getRed())) * current_Pos) + ((1f / 255f) * b.getRed());
+    		float temp_G = ((((1f / 255) * a.getGreen()) - ((1f / 255) * b.getGreen())) * current_Pos) + ((1f / 255f) * b.getGreen());
+    		float temp_B = ((((1f / 255) * a.getBlue()) - ((1f / 255) * b.getBlue())) * current_Pos) + ((1f / 255f) * b.getBlue());
+    		
+    		current_Hue = new Color(temp_R, temp_G, temp_B, 1f);
+    	}
+    	
+    	if(current_Hue == null)
+    	{
+    		return Color.white;
+    	}
+    	else
+    	{
+    		return current_Hue;
+    	}
+    }
 
-        for (int i = 0; i < allSpiros.size(); ++i) //For all the spiros
+    public BufferedImage getImage() 
+    {
+    	buffer.setColor(getColour());
+    	
+    	if(last_Time != 0f)
+    	{
+    		delta_Time = System.nanoTime() - last_Time;
+    	}
+    	
+    	last_Time = System.nanoTime();
+    	
+    	int pixels_To_Draw = (int)(pixels_Per_Sec * delta_Time);
+    	
+    	paintIterator += pixels_To_Draw;
+    	
+        if (current_Spiro.x_Vals.size() <= paintIterator) //If the paint iterator hasn't exceeded the number of pixels the shape actually contains
         {
-            if (allSpiros.get(i).x_Vals.size() <= paintIterator) //If the paint iterator hasn't exceeded the number of pixels the shape actually contains
-            {
-                for (int j = 0; j < paintIterator; ++j) //For all positions up to the paint iterator
-                {
-                    int x_Pos = allSpiros.get(i).x_Vals.get(j); //Get the x and y values of the current pixel coordinate
-                    int y_Pos = allSpiros.get(i).y_Vals.get(j);
+             for (int j = 0; j < paintIterator; ++j) //For all positions up to the paint iterator
+             {
+                    int x_Pos = current_Spiro.x_Vals.get(j); //Get the x and y values of the current pixel coordinate
+                    int y_Pos = current_Spiro.y_Vals.get(j);
 
                     buffer.drawLine(x_Pos, y_Pos, x_Pos+1, y_Pos+1); //Draw the pixel
-                }
-            }
+             }
         }
 
-        ++paintIterator; //Increase the paint iterator
-
-        g.drawImage(offScreen, 0, 0, null); //Draw the image
+        return offScreen; //Draw the image
     }
 
-    public void createSpiroObject(int R, int r, int o) {
-        spiroObject mySpiroObject = new spiroObject(R, r, o);
-        allSpiros.add(mySpiroObject);
-
-    }
-
-    class spiroObject //Spiro object
+    class SpiroObject //Spiro object
     {
-
         int radius_R, radius_r, oVal;
+        ArrayList<Color> spiro_Colours = new ArrayList<Color>();
         ArrayList<Integer> x_Vals = new ArrayList<Integer>();
         ArrayList<Integer> y_Vals = new ArrayList<Integer>();
 
-        spiroObject(int R, int r, int o) //Takes constructor for radius of large circle, small circle, and small circle offset
+        SpiroObject(int R, int r, int o, Color[] colours) //Takes constructor for radius of large circle, small circle, and small circle offset
         {
             radius_R = R;
             radius_r = r;
             oVal = o;
+            
+            for(int i = 0; i < colours.length; ++i)
+            {
+            	spiro_Colours.add(colours[i]);
+            }
+            
+            spiro_Colours.add(colours[0]);
+
             calculatePositions(); //Auto call the calculate positions method
         }
 
-        private void calculatePositions() {
-            int t = 0; //Say some number is 0
+        private void calculatePositions() 
+        {
+        	 int t = 0; //Say some number is 0
+             
+             while(true) //Until break occurs
+             {
+                     int x_Temp = (radius_R + radius_r) * (int)Math.cos((double)t) - (radius_r + oVal) * (int)Math.cos((double)(((radius_R + radius_r)/radius_r) * t)); //Get the x coordinate for this value of t
+                     int y_Temp = (radius_R + radius_r) * (int)Math.sin((double)t) - (radius_r + oVal) * (int)Math.sin((double)(((radius_R + radius_r)/radius_r) * t)); //Same for y
 
-            int x_Temp ;
-            int y_Temp;
-            
-            do{
-                x_Temp = (radius_R + radius_r) * (int) Math.cos((double) t) - (radius_r + oVal) * (int) Math.cos((double) (((radius_R + radius_r) / radius_r) * t)); //Get the x coordinate for this value of t
-                y_Temp = (radius_R + radius_r) * (int) Math.sin((double) t) - (radius_r + oVal) * (int) Math.sin((double) (((radius_R + radius_r) / radius_r) * t)); //Same for y
-                
-                x_Vals.add(x_Temp); //Add the x and y coordinates to the arrays
-                y_Vals.add(y_Temp);
-                
-                ++t; //Increment t by 1
-            }while (!(x_Temp == x_Vals.get(0) && y_Temp == y_Vals.get(0))); //If the two values equal the original value, we can stop
+                     if(x_Temp == x_Vals.get(0) && y_Temp == y_Vals.get(0)) //If the two values equal the original value, we can stop
+                     {
+                             break; //Break out of the loop
+                     }
+                    
+                     x_Vals.add(x_Temp); //Add the x and y coordinates to the arrays
+                     y_Vals.add(y_Temp);
+                    
+                     ++t; //Increment t by 1
+             }
         }
     }
 }
